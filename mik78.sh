17087c82016d612516dde9e51a0ae97c910b7a60
Defm <defm.kopcap@gmail.com>
2023-05-13 21:52:31 +0300

    Initial mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -1,20 +1,41 @@
 #!/bin/bash -e
 
 echo
-echo "=== azadrah.org ==="
-echo "=== https://github.com/azadrahorg ==="
 echo "=== MikroTik 7 Installer ==="
 echo
 sleep 3
 
-wget https://download.mikrotik.com/routeros/7.8/chr-7.8.img.zip -O chr.img.zip  && \
-gunzip -c chr.img.zip > chr.img  && \
+# Must be root !
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+echo "Preparation ..."
+apt install unzip -y
+
+# Latest Stable
+CHR_VERSION=7.8
+
+wget -qO routeros.zip https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip && \
+unzip routeros.zip && \
+rm -rf routeros.zip
+
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
 ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
 ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+
+mount -o loop,offset=512 chr-$CHR_VERSION.img /mnt
+
+ls /mnt
+
+echo "" > /mnt/rw/autorun.scr
+
+umount /mnt
+
 echo u > /proc/sysrq-trigger && \
-dd if=chr.img bs=1M of=/dev/$STORAGE iflag=fullblock && \
+dd if=chr-$CHR_VERSION.img of=/dev/$STORAGE && \
 echo "sync disk" && \
 echo s > /proc/sysrq-trigger && \
 echo "Sleep 5 seconds" && \
